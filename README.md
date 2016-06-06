faa410b545ef0384e55427c2cac8286adcd63d1d
abhijitsingh86 <enggabhijit.singh@gmail.com>
2016-06-06 23:41:58 +0200

    initial commit without node modules

---
+++ README.md
@@ -0,0 +1,9 @@
+# Conference Mangement System
+Add your Code.
+To run
+npm install
+node server.js
+
+open http://localhost:3000
+
+Enjoy:)
---
+++ controllers/upload.controller.js
@@ -0,0 +1,30 @@
+ar express = require('express');    //Express Web Server 
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+var router = express.Router();
+
+var app = express();
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
+
+ 
+app.route('/upload')
+    .post(function (req, res, next) {
+
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            fstream = fs.createWriteStream(__dirname + '/img/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {    
+                console.log("Upload Finished of " + filename);              
+                res.redirect('back');           //where to go next
+            });
+        });
+    });
+
+module.exports = router;
