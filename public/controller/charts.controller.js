5740d880cb6a4f188a89f1080561d14e8c9bfdc3
Manish Kumar <manishk.1991@gmail.com>
2016-09-07 23:09:53 +0200

    charts updated

--- public/controller/charts.controller.js
+++ public/controller/charts.controller.js
@@ -3,8 +3,8 @@ app.controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
     $scope.series = ['Accepted Submissions', 'Open Submissions'];
     $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
+        [64, 58, 81, 82, 57, 56, 41],
+        [27, 49, 41, 18, 85, 28, 91]
     ];
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
@@ -13,8 +13,8 @@ app.controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
     // Simulate async data update
     $timeout(function () {
         $scope.data = [
-            [28, 48, 40, 19, 86, 27, 90],
-            [65, 59, 80, 81, 56, 55, 40]
+            [29, 49, 41, 18, 87, 28, 91],
+            [66, 58, 81, 82, 55, 54, 41]
         ];
     }, 3000);
 
