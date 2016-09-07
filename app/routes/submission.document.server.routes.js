d05d47694aa18da43756fe4ae7b73f77daae215c
ksaurabh02 <ksaurabh.02@gmail.com>
2016-09-08 00:48:52 +0530

    Comments and node_module removed from tracking files.

--- app/routes/submission.document.server.routes.js
+++ app/routes/submission.document.server.routes.js
@@ -2,19 +2,22 @@ var submissionDocument =  require('../controllers/submission.document.server.con
 var users =require('../../app/controllers/users.server.controller');
 module.exports = function(app) {
 
+    // Upload the file as userid_eventidsubmisson.pdf
     app.route('/upload').
     post(submissionDocument.upload);
 
+    //Create a new submission document, get list of all document
     app.route('/subDocument').
     post(submissionDocument.create).
     get(submissionDocument.list);
 
-
+    //Read the details of document, update the document, delete a document.
     app.route('/subDocument/:subDocumentId').
     get(submissionDocument.read).
     put(submissionDocument.update).
     delete(submissionDocument.delete);
-    
+
+    //download a file
     app.route('/subDocument/:userId/:subDocId').
         post(submissionDocument.download);
     /*
@@ -37,6 +40,8 @@ module.exports = function(app) {
     app.route('/subDocumentByUserId')
         .post(submissionDocument.listOfSubmissionDocumentForUser);
 
+
+    //list Of AllSubmissionDocs For an Event
     app.route('/listOfAllSubmissionDocsForEvent/:eventId').
     get(submissionDocument.listOfAllSubmissionsForEvent);
 
